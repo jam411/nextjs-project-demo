c24886d5584448cc4c01a8af1e704db43be4fed7
jam411 <ryoji-s@weseek.co.jp>
2022-09-18 01:39:59 +0900

    create _document.tsx for style

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  compiler: {
+    // Enable styledComponents
+    styledComponents: true,
+  }
 }
 
 module.exports = nextConfig
